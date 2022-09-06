33024ad92752df76936b6df345eef9a500f56d2a
Wajeeh Haider <75931599+Wajeeh-Haider@users.noreply.github.com>
2022-09-06 03:49:43 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const data = require("./api/data.json");
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(data);
+});
+
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const food = data.find((food) => food.id === Number(id));
+  res.send(food);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port http://localhost:${PORT}`);
+});
