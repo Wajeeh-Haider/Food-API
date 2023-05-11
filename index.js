cf1c88f2fea1385332aa1bf15ecacbd01afa6e98
Wajeeh Haider <75931599+Wajeeh-Haider@users.noreply.github.com>
2023-05-11 09:54:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const data = require("./data.json");
 const dotenv = require("dotenv");
 dotenv.config();
-
 app.use(cors());
 app.use(express.json());
 
@@ -16,6 +15,30 @@ app.get("/:id", (req, res) => {
   const id = req.params.id;
   const food = data.find((food) => food.id === Number(id));
   res.send(food);
+});
+
+app.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const index = data.findIndex((food) => food.id === Number(id));
+  if (index !== -1) {
+    const updatedFood = { ...data[index], ...req.body };
+    data[index] = updatedFood;
+    res.send(`Food with ID ${id} updated`);
+  } else {
+    res.status(404).send(`Food with ID ${id} not found`);
+  }
+});
+
+app.patch("/:id", (req, res) => {
+  const id = req.params.id;
+  const index = data.findIndex((food) => food.id === Number(id));
+  if (index !== -1) {
+    const updatedFood = { ...data[index], ...req.body };
+    data[index] = updatedFood;
+    res.send(`Food with ID ${id} updated`);
+  } else {
+    res.status(404).send(`Food with ID ${id} not found`);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
